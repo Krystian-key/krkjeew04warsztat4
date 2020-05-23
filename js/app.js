ab145a179bbfb277e90e87d77379b18da059a981
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-05-23 19:16:18 +0200

    Warsztat ukończony.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-$(function () {
-    //curl -X POST -i -H "Content-Type: application/json" -d '{"isbn":"34321","title":"Thinking in Java", "publisher":"Helion","type":"programming","author":"Bruce Eckel"}' http://localhost:8282/books
-    //curl -X POST -i -H "Content-Type: application/json" -d '{"isbn":"34321","title":"Potop", "publisher":"Ossolineum","type":"belerystyka","author":"Sienkiewicz"}' http://localhost:8282/books
+$(function(){
+    // add new book using cURL:
+    // curl -X POST -i -H "Content-Type: application/json" -d '{"isbn":"34321","title":"Potop","publisher":"Ossolineum","type":"beletrystyka","author":"Sienkiewicz"}' http://localhost:8282/books
+
     var bookListDiv = $(".book-list");
     bookListDiv.on("click", ".book-title", handleTitleClick);
-    bookListDiv.on("click", ".delete-book-btn", handlDelBtnClick);
+    bookListDiv.on("click", ".delete-book-btn", handleDelBtnClick);
+
     var addBookForm = $(".add-book-form");
     addBookForm.on("submit", handleAddBookSubmit);
+
     refreshBookList();
-    function refreshBookList() {
-        // var bookArr = getFromServer();
+
+    function refreshBookList(){
         renderBookList(bookListDiv);
     }
-    function renderBookList(renderingPoint) {
-        $.ajax({
-            url: "http://localhost:8282/books",
-            type: "GET",
-            dataType: "json",
-        }).done(function (booksArr) {
+
+    function renderBookList(renderingPoint){
+        var url = "http://localhost:8282/books";
+
+        function getBookListSuccess(booksArr){
             renderingPoint.empty();
-            for (var i = 0; i < booksArr.length; i++) {
+
+            for(var i=0; i<booksArr.length; i++){
                 var book = booksArr[i];
+
                 var descriptionDiv = $('<div class="description">');
                 var delBtn = $('<button class="delete-book-btn">Usuń</button>');
                 var titleDiv = $('<div class="book-title">');
@@ -27,40 +31,47 @@ $(function () {
                 titleDiv.data("id", book.id);
                 titleDiv.append(delBtn);
                 titleDiv.append(descriptionDiv);
+
                 renderingPoint.append(titleDiv);
             }
-        }).fail(function (xhr, status, err) {
-            console.log(xhr, status, err);
-        });
-    }
+        } // getBookListSuccess
+
+        sendGenericRequest(url, "GET", undefined, getBookListSuccess);
+    } // renderBookList
+
     function handleTitleClick() {
         var thisTitle = $(this);
-        var id = $(this).data("id");
-        $.ajax({
-            url: "http://localhost:8282/books/" + id,
-            type: "GET",
-            dataType: "json",
-        }).done(function (book) {
+        var id = thisTitle.data("id");
+        var url = "http://localhost:8282/books/" + id;
+
+        function getBookSuccess(book){
             var descriptionDiv = thisTitle.find(".description");
             descriptionDiv.empty();
+
             var authorDiv = $("<div>");
             authorDiv.text("Author: " + book.author);
+
             var publisherDiv = $("<div>");
             publisherDiv.text("publisher: " + book.publisher);
+
             var typeDiv = $("<div>");
             typeDiv.text("type: " + book.type);
+
             var isbnDiv = $("<div>");
             isbnDiv.text("isbn: " + book.isbn);
+
             descriptionDiv.append(authorDiv);
             descriptionDiv.append(publisherDiv);
             descriptionDiv.append(typeDiv);
             descriptionDiv.append(isbnDiv);
+
             descriptionDiv.slideDown();
-        }).fail(function (xhr, status, err) {
-            console.log(xhr, status, err);
-        });
-    }
-    function handleAddBookSubmit() {
+        } // getBookSuccess
+
+        sendGenericRequest(url, "GET", undefined, getBookSuccess);
+    } // handleTitleClick
+
+    function handleAddBookSubmit(){
         var book = {
             title : this.elements.title.value,
             author : this.elements.author.value,
@@ -68,31 +79,38 @@ $(function () {
             type : this.elements.type.value,
             isbn : this.elements.isbn.value,
         }
-        console.log(book);
-        $.ajax({
-            url: "http://localhost:8282/books",
-            type: "POST",
-            data: JSON.stringify(book),
-            contentType : "application/json; charset=urf-8",
-            dataType: "json",
-        }).done(function () {
-            refreshBookList();
-        }).fail(function (xhr, status, err) {
-            console.log(xhr, status, err);
-        });
+
+        var url = "http://localhost:8282/books";
+
+        sendGenericRequest(url, "POST", book, refreshBookList);
+
         return false;
-    }
-    function handlDelBtnClick(event) {
+    } // handleAddBookSubmit
+
+    function handleDelBtnClick(event){
         event.stopPropagation();
-        var  thisTitle = $(this).parent();
+
+        var thisTitle = $(this).parent();
         var id = thisTitle.data("id");
+        var url = "http://localhost:8282/books/" + id;
+
+        sendGenericRequest(url, "DELETE", undefined, refreshBookList);
+    } // handleDelBtnClick
+
+    function sendGenericRequest(url, type, data, successHandlerFn){
         $.ajax({
-            url: "http://localhost:8282/books/" + id,
-            type: "DELETE",
-        }).done(function () {
-            refreshBookList();
-        }).fail(function (xhr, status, err) {
+            url : url,
+            type : type,
+            data : data === undefined ? "" : JSON.stringify(data),
+            contentType : "application/json; charset=utf-8",
+            dataType : "json",
+        }).done(function(dataReturnedByServer){
+            if(successHandlerFn !== undefined){
+                successHandlerFn(dataReturnedByServer);
+            }
+        }).fail(function(xhr, status, err){
             console.log(xhr, status, err);
         });
-    }
+    } // sendGenericRequest
+
 }) // DOMContentLoaded
