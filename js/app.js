57cd6f4e94b2b2bf41f06210c633a77590e28b6e
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-05-23 16:17:22 +0200

    task number 6, Delete button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(function () {
     //curl -X POST -i -H "Content-Type: application/json" -d '{"isbn":"34321","title":"Potop", "publisher":"Ossolineum","type":"belerystyka","author":"Sienkiewicz"}' http://localhost:8282/books
     var bookListDiv = $(".book-list");
     bookListDiv.on("click", ".book-title", handleTitleClick);
+    bookListDiv.on("click", ".delete-book-btn", handlDelBtnClick);
+    var addBookForm = $(".add-book-form");
+    addBookForm.on("submit", handleAddBookSubmit);
     refreshBookList();
     function refreshBookList() {
         // var bookArr = getFromServer();
@@ -18,9 +21,11 @@ $(function () {
             for (var i = 0; i < booksArr.length; i++) {
                 var book = booksArr[i];
                 var descriptionDiv = $('<div class="description">');
+                var delBtn = $('<button class="delete-book-btn">Usuń</button>');
                 var titleDiv = $('<div class="book-title">');
                 titleDiv.text(book.title);
                 titleDiv.data("id", book.id);
+                titleDiv.append(delBtn);
                 titleDiv.append(descriptionDiv);
                 renderingPoint.append(titleDiv);
             }
@@ -45,12 +50,47 @@ $(function () {
             var typeDiv = $("<div>");
             typeDiv.text("type: " + book.type);
             var isbnDiv = $("<div>");
-            isbnDiv.text("isbn: " + book.type);
+            isbnDiv.text("isbn: " + book.isbn);
             descriptionDiv.append(authorDiv);
             descriptionDiv.append(publisherDiv);
             descriptionDiv.append(typeDiv);
             descriptionDiv.append(isbnDiv);
             descriptionDiv.slideDown();
+        }).fail(function (xhr, status, err) {
+            console.log(xhr, status, err);
+        });
+    }
+    function handleAddBookSubmit() {
+        var book = {
+            title : this.elements.title.value,
+            author : this.elements.author.value,
+            publisher : this.elements.publisher.value,
+            type : this.elements.type.value,
+            isbn : this.elements.isbn.value,
+        }
+        console.log(book);
+        $.ajax({
+            url: "http://localhost:8282/books",
+            type: "POST",
+            data: JSON.stringify(book),
+            contentType : "application/json; charset=urf-8",
+            dataType: "json",
+        }).done(function () {
+            refreshBookList();
+        }).fail(function (xhr, status, err) {
+            console.log(xhr, status, err);
+        });
+        return false;
+    }
+    function handlDelBtnClick(event) {
+        event.stopPropagation();
+        var  thisTitle = $(this).parent();
+        var id = thisTitle.data("id");
+        $.ajax({
+            url: "http://localhost:8282/books/" + id,
+            type: "DELETE",
+        }).done(function () {
+            refreshBookList();
         }).fail(function (xhr, status, err) {
             console.log(xhr, status, err);
         });
